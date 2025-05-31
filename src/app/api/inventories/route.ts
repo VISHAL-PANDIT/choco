94c2a6c0d4c84f395cc395a982e99863106eed74
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-05-31 17:35:11 +0530

    get all inventories

--- src/app/api/inventories/route.ts
+++ src/app/api/inventories/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schema";
+import { inventories, products, warehouses } from "@/lib/db/schema";
 import { inventorySchema } from "@/lib/validators/inventorySchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
   const requestData = await request.json();
@@ -21,6 +22,30 @@ export async function POST(request: Request) {
         error: error,
       },
       { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const allInventories = await db
+      .select({
+        id: inventories.id,
+        sku: inventories.sku,
+        warehouseId: warehouses.name,
+        product: products.name,
+      })
+      .from(inventories)
+      .orderBy(desc(inventories.id))
+      .leftJoin(warehouses, eq(inventories.warehouseId, warehouses.id))
+      .leftJoin(products, eq(inventories.productId, products.id));
+    return Response.json({ allInventories });
+  } catch (error) {
+    return Response.json(
+      { message: "Failed to fetch inventory item", error: error },
+      {
+        status: 500,
+      }
     );
   }
 }
