3d3a2ebe020e14fe499db44a0c3f021f2c71e438
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-05-31 17:27:34 +0530

    create inventories

---
+++ src/app/api/inventories/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db/db";
+import { inventories } from "@/lib/db/schema";
+import { inventorySchema } from "@/lib/validators/inventorySchema";
+
+export async function POST(request: Request) {
+  const requestData = await request.json();
+
+  let validatedData;
+  try {
+    validatedData = await inventorySchema.parse(requestData);
+  } catch (error) {
+    return Response.json({ message: error }, { status: 400 });
+  }
+  try {
+    await db.insert(inventories).values(validatedData);
+    return Response.json({ message: "OK" }, { status: 201 });
+  } catch (error) {
+    return Response.json(
+      {
+        message: "Failed to store the inventory into the database",
+        error: error,
+      },
+      { status: 500 }
+    );
+  }
+}
