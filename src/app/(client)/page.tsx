0615edd49de64392923e88e46c8dfab00ea60928
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-07 05:56:13 +0530

    Remove all the unwanted files from code

---
+++ src/app/(client)/page.tsx
@@ -0,0 +1,18 @@
+
+
+
+const HomePage = () => {
+    return (
+        <>
+            {/* <Header />
+            <Hero />
+            <SpecialProducts />
+            <About />
+            <Products />
+            <NewsLetter />
+            <Footer /> */}
+        </>
+    );
+};
+
+export default HomePage;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = () => {
-  return (
-    <h1>Hello form Choco</h1>
-  )
-}
-
-export default Page
