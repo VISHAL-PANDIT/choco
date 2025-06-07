7cfeea37e6abcf10f363f28f24d5a2103fbe64ce
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-07 18:03:14 +0530

    render products

--- src/app/(client)/_components/Products.tsx
+++ src/app/(client)/_components/Products.tsx
@@ -1,23 +1,67 @@
-"use client"
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/http/api";
+import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const CLientProducts = () => {
-  return (
-    <section className='bg-[#f5f5f5] px-5 py-14 md:py-20'>
-        <div className='mx-auto max-w-6xl'>
-            <div className="flex items-center justify-center gap-5">
-                <div className='h-[2px] bg-amber-900 w-[30%]'/>
-                <h2 className="text-3xl font-bold tracking-tight text-amber-900">
-                    Special Products
-                </h2>
-                <div className='h-[2px] bg-amber-900 w-[30%]'/>
-            </div>
-            <div className='mt-20 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-
-            </div>
-        </div>
-    </section>
-  )
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  image: string;
 }
 
-export default CLientProducts
+const Products = () => {
+  const { data: products = [] } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const response = await getAllProducts();
+      return response as Product[];
+    },
+    staleTime: 10 * 1000,
+  });
+
+  return (
+    <section className="bg-[#f5f5f5] px-5 py-14 md:py-20">
+      <div className="mx-auto max-w-6xl">
+        <div className="flex items-center justify-center gap-5">
+          <div className="h-[2px] bg-amber-900 w-[30%]" />
+          <h2 className="text-3xl font-bold tracking-tight text-amber-900">
+            Products
+          </h2>
+          <div className="h-[2px] bg-amber-900 w-[30%]" />
+        </div>
+        <div className="mt-20 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((product: Product) => (
+            <div key={product.id} className="flex flex-col items-start justify-center gap-5">
+              <Image
+                src={`/assets/${product.image}`}
+                alt={product.name}
+                width={300}
+                height={300}
+                sizes="100vw"
+                style={{ width: "100%" }}
+                className="aspect-square rounded-t-md object-cover shadow-lg hover:cursor-pointer"
+              />
+              <div className="w-full">
+                <p className="text-lg font-semibold text-amber-900">{product.name}</p>
+                <div className="mt-1 space-x-2">
+                  <span className="font-bold">${product.price}</span>
+                </div>
+                <Link href={`/product/${product.id}`}>
+                  <Button size={"sm"} className="mt-5 w-full bg-amber-900 hover:bg-amber-800 active:bg-amber-700">
+                    Buy Now
+                  </Button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Products;
