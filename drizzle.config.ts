58669b09c2383c71599fb25a134541c56406fe78
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-09 17:34:09 +0530

    changes for vercel

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ export default defineConfig({
     schema: './src/lib/db/schema.ts',
     out: './drizzle',
     dialect: 'postgresql',
-    dbCredentials:{
-        url: process.env.DATABASE_URL as string,
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL as string,
     }
 })
