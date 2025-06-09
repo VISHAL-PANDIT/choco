a73ff5b0cef39a30c59c42ed7262614bea1003f1
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-09 18:15:44 +0530

    changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,6 @@ export default defineConfig({
     out: './drizzle',
     driver: 'pg',
     dbCredentials: {
-        host: process.env.POSTGRES_HOST || 'localhost',
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DATABASE,
-        port: Number(process.env.POSTGRES_PORT) || 5432,
+        connectionString: process.env.DATABASE_URL as string
     }
 } satisfies Config);
