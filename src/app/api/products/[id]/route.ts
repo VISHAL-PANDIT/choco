149663b212637ee2c41b8724ad0b152b5ce93149
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-09 06:25:59 +0530

    new changes for deployment

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,12 +1,13 @@
 import { db } from "@/lib/db/db";
 import { products } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  context: { params: { id: string } }
 ) {
-  const id = params.id;
+  const id = context.params.id;
   try {
     const product = await db
       .select()
