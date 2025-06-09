e0ca85778295712b4484cc55e8fc152d875d6807
VISHAL PANDIT <vishalpandit1172004@gmail.com>
2025-06-09 06:28:18 +0530

    correct the mismatch error

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,12 +1,12 @@
 import { db } from "@/lib/db/db";
 import { products } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
+export const GET = async (
   request: NextRequest,
   context: { params: { id: string } }
-) {
+) => {
   const id = context.params.id;
   try {
     const product = await db
@@ -15,12 +15,12 @@ export async function GET(
       .where(eq(products.id, Number(id)))
       .limit(1);
     if (!product.length) {
-      return Response.json({ message: "Product not found" }, { status: 400 });
+      return NextResponse.json({ message: "Product not found" }, { status: 400 });
     }
 
-    return Response.json(product[0])
+    return NextResponse.json(product[0])
   } catch (error) {
-    return Response.json(
+    return NextResponse.json(
       { message: "Product not found", error: error },
       { status: 400 }
     );
